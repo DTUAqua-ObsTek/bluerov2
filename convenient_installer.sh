43efd88609df31e4afa5d66d324ef75ffe6e92c7
DTUAqua-ObsTek <88486626+DTUAqua-ObsTek@users.noreply.github.com>
2022-02-09 10:23:40 +0100

    Update convenient_installer.sh

--- convenient_installer.sh
+++ convenient_installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 INSTALL_PATH=$HOME/rov_ros_ws
-
+sudo apt update
 echo "Installing ROS..."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl # if you haven't already installed curl
@@ -11,6 +11,7 @@ sudo apt install ros-noetic-desktop-full
 grep -qxF 'source /opt/ros/noetic/setup.bash' ~/.bashrc || echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-catkin-tools
+sudo rosdep init
 rosdep update
 echo "ROS Installed."
 
